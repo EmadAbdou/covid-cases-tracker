eb70fa8c5bef3a34ce8833b79f3d554a43531e8e
EmadAbdou <emad_abdou7@yahoo.com>
2021-01-19 21:52:32 +0200

    Finish App
    - Implement Logic
    - Connect to API's
    - Work on responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css'
+import { fetchData } from './api';
+import coronaImage from './images/image.png'
 
 export class App extends Component {
+
+    state = {
+        data: {},
+        country: ''
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData })
+    }
+
+    handleCountryChange = async(country) => {
+        console.log(country);
+        const fetchedData = await fetchData(country);
+        console.log(fetchedData);
+        this.setState({ data: fetchedData, country: country });
+
+    }
+
     render() {
+        const { data, country} = this.state;
         return (
-            <div>
-                <h1>APP</h1>
+            <div className={styles.container}>
+                <img src={coronaImage} className={styles.image} alt="COVID - 19" />
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
             </div>
         )
     }
